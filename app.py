79ebcda5b7c90a5f24ca0f21df4113494ce3c11e
Antony Champane <a.champagne@wonder-studio.com>
2025-02-06 15:52:22 +0100

    First Streamlit commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import plotly as px
+import streamlit as st
+
+
+st.title("Data Viz Web App")
+st.header("Part 1: Data Exploration")
+st.write("In this section, we will explore the Altair cars dataset.")
+st.markdown("*Further resources [here](https://altair-viz.github.io/gallery/selection_histogram.html)*")
